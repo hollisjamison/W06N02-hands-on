c6e291058f25efcfe219fea14cec9ba004fe772e
Hollis Jamison <hollisjamison@gmail.com>
2023-04-02 19:18:01 -0400

    intial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const movies = [
+  {
+    name: "Ghostbusters",
+    director: "Ivan Reitman",
+    year: 1984,
+    posterImage: "ghostBusters.jpg",
+    synopsis:
+      "Three parapsychologists forced out of their university funding setup shop as a unique ghost removal service in New York City, attracting frightened yet skeptical customers.",
+  },
+  {
+    name: "Back to the Future",
+    director: "Robert Zemeckis",
+    year: 1985,
+    posterImage: "backToTheFuture.webp",
+    synopsis:
+      "A teenager is sent back in time to the 1950s by a time-traveling DeLorean car invented by his eccentric scientist friend. He must make sure his parents fall in love in order to return to his own time.",
+  },
+  {
+    name: "Titanic",
+    director: "James Cameron",
+    year: 1997,
+    posterImage: "titanic.jpg",
+    synopsis:
+      "A young woman falls in love with a poor artist aboard the luxurious, ill-fated R.M.S. Titanic.",
+  },
+];
